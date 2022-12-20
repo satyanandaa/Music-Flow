449c648948af1ff9bc1246ef3d42b49bd9ce9c7b
satyanandaa <101556979+satyanandaa@users.noreply.github.com>
2022-12-20 18:34:42 +0700

    Delete src directory

--- src/utils/dbConnection.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Database connection 
-$conn = mysqli_connect('db', 'root', 'root', 'myspotify');
-
-mysqli_set_charset($conn, "utf8");
-
-if (!$conn) {
-    echo mysqli_connect_error();
-}
