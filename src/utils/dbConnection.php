ae13402064b3e10a1f124662b6435234d8359c83
satyanandaa <101556979+satyanandaa@users.noreply.github.com>
2023-01-02 18:05:13 +0700

    Update dbConnection.php

--- src/utils/dbConnection.php
+++ src/utils/dbConnection.php
@@ -1,6 +1,6 @@
 <?php
 // Database connection 
-$conn = mysqli_connect('db', 'root', 'root', 'myspotify');
+$conn = mysqli_connect('db', 'root', 'root', 'musicflow');
 
 mysqli_set_charset($conn, "utf8");
 
